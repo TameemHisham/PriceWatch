830a1c6e94d6250cc1c7ca47b7ee96085982d865
TameemHisham <alkhliefattameem@gmail.com>
2026-07-11 04:50:29 +0400

    restart

--- backend/src/main/java/com/tameem/pricewatch/scraper/ProductScraper.java
+++
@@ -1,7 +0,0 @@
-package com.tameem.pricewatch.scraper;
-
-public interface ProductScraper {
-    String storeName();
-    boolean supports(String url);
-    ProductData scrape(String url);
-}
--- backend/src/main/java/com/tameem/pricewatch/scraper/ScrapeException.java
+++
@@ -1,6 +0,0 @@
-package com.tameem.pricewatch.scraper;
-
-public class ScrapeException extends RuntimeException {
-    public ScrapeException(String message) { super(message); }
-    public ScrapeException(String message, Throwable cause) { super(message, cause); }
-}
